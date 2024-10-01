13bac3f0a91244a792ff4993f4f1807f3f57e36d
Spencer Winward <srwinz@gmail.com>
2024-10-01 09:49:55 -0600

    ChessRuleBook Interface

---
+++ shared/src/main/java/chess/rulebook/ChessRuleBook.java
@@ -0,0 +1,16 @@
+package chess.rulebook;
+
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.Collection;
+
+public interface ChessRuleBook {
+    Collection<ChessMove> validMoves(ChessPosition startPosition);
+    boolean isBoardValid(ChessBoard board);
+    boolean isInCheck(ChessGame.TeamColor teamColor);
+    boolean isInCheckmate(ChessGame.TeamColor teamColor);
+    boolean isInStalemate(ChessGame.TeamColor teamColor);
+}
