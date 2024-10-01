b9cb0fae87f90dcf2eb36111860aa442fb0736fb
Spencer Winward <srwinz@gmail.com>
2024-10-01 09:55:21 -0600

    FIDERuleBook stub

---
+++ shared/src/main/java/chess/rulebook/FIDERuleBook.java
@@ -0,0 +1,32 @@
+package chess.rulebook;
+
+import chess.ChessBoard;
+import chess.ChessGame;
+import chess.ChessMove;
+import chess.ChessPosition;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+
+public class FIDERuleBook implements ChessRuleBook{
+    public Collection<ChessMove> validMoves(ChessPosition startPosition) {
+        return new HashSet<>();
+    }
+
+    public boolean isBoardValid(ChessBoard board) {
+        return false;
+    }
+
+    public boolean isInCheck(ChessGame.TeamColor teamColor) {
+        return false;
+    }
+
+    public boolean isInCheckmate(ChessGame.TeamColor teamColor) {
+        return false;
+    }
+
+    public boolean isInStalemate(ChessGame.TeamColor teamColor) {
+        return false;
+    }
+}
