5a71ea4610ed720e2e3d4e723f6d9af3c0ddd45a
Spencer Winward <srwinz@gmail.com>
2024-10-21 17:50:47 -0600

    stub memoryGameDAO but better

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -1,4 +1,29 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.GameData;
+
+import java.util.Collection;
+import java.util.HashSet;
+
 public class MemoryGameDAO {
+    public GameData createGame(String authToken) throws DataAccessException {
+        return new GameData(1, "w", "b", "test", new ChessGame());
+    }
+
+    public GameData getGame(int gameID) throws DataAccessException {
+        return new GameData(1, "w", "b", "test", new ChessGame());
+    }
+
+    public Collection<GameData> listGames(String authToken) throws DataAccessException {
+        return new HashSet<GameData>();
+    }
+
+    public GameData updateGame(String authToken, int gameID) throws DataAccessException {
+        return new GameData(1, "w", "b", "test", new ChessGame());
+    }
+
+    public void clear() throws DataAccessException {
+
+    }
 }
