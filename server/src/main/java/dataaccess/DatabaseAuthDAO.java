52d299b4fe5a450d4642abe63653f98583685ad6
Spencer Winward <srwinz@gmail.com>
2024-11-01 09:50:59 -0600

    full stub DBAuthDAO

--- server/src/main/java/dataaccess/DatabaseAuthDAO.java
+++ server/src/main/java/dataaccess/DatabaseAuthDAO.java
@@ -1,4 +1,19 @@
 package dataaccess;
 
-public class DatabaseAuthDAO {
+import model.AuthData;
+
+public class DatabaseAuthDAO implements AuthDAO {
+    public AuthData addAuth(AuthData a) throws DataAccessException {
+        return new AuthData("token", "user");
+    }
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return new AuthData("token", "user");
+    }
+    public void deleteAuth(AuthData authData) throws DataAccessException {
+
+    }
+
+    public void deleteAllAuths() throws DataAccessException {
+
+    }
 }
