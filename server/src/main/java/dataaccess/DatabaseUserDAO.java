e6fc202ef2197f996f05ed71575553fb75e8c0f8
Spencer Winward <srwinz@gmail.com>
2024-11-01 09:52:15 -0600

    full stub DBUserDAO

--- server/src/main/java/dataaccess/DatabaseUserDAO.java
+++ server/src/main/java/dataaccess/DatabaseUserDAO.java
@@ -1,4 +1,16 @@
 package dataaccess;
 
-public class DatabaseUserDAO {
+import model.UserData;
+
+public class DatabaseUserDAO implements UserDAO {
+    public UserData addUser(UserData u) throws DataAccessException {
+        return new UserData("name", "password", "email");
+    }
+    public UserData getUser(String username) throws DataAccessException {
+        return new UserData("name", "password", "email");
+    }
+
+    public void deleteAllUsers() throws DataAccessException {
+
+    }
 }
