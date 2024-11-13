74e2ae516d00a9b0bf9c35fa89b4cc874882fbf2
Spencer Winward <srwinz@gmail.com>
2024-11-13 11:56:59 -0700

    repl

---
+++ client/src/main/java/client/Repl.java
@@ -0,0 +1,43 @@
+package client;
+
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
+public class Repl {
+    private final ChessClient client;
+
+    public Repl(String serverUrl) {
+        client = new ChessClient(serverUrl);
+    }
+
+    public void run() {
+        System.out.println("Welcome to Chess. Type help to get started.");
+//        System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+//    public void notify(Notification notification) {
+//        System.out.println(RED + notification.message());
+//        printPrompt();
+//    }
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET_TEXT_COLOR + "[" + client + "]>>> " + SET_TEXT_COLOR_GREEN);
+    }
+}
