9730e4c5195e4200fd646098a3e12d125c6e6273
Spencer Winward <srwinz@gmail.com>
2024-12-02 16:50:49 -0700

    Websocket Stubs p2

--- server/src/main/java/handler/WebSocketHandler.java
+++ server/src/main/java/handler/WebSocketHandler.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.ConnectionManager;
 
 import java.io.IOException;
 
