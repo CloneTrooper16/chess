f731c7d7b1141937f6d105a4d134144be1e0f8d4
Spencer Winward <srwinz@gmail.com>
2024-12-03 14:34:35 -0700

    ws communicator start

--- client/src/main/java/client/websocket/WebSocketCommunicator.java
+++ client/src/main/java/client/websocket/WebSocketCommunicator.java
@@ -1,4 +1,81 @@
 package client.websocket;
 
-public class WebSocketCommunicator {
+import com.google.gson.Gson;
+import exception.ResponseException;
+import websocket.commands.UserGameCommand;
+
+import javax.websocket.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class WebSocketCommunicator extends Endpoint {
+    private Session session;
+    private NotificationHandler notificationHandler;
+
+    public WebSocketCommunicator(String url, NotificationHandler notificationHandler) throws ResponseException {
+        try {
+            url = url.replace("http", "ws");
+            URI socketURI = new URI(url + "/ws");
+            this.notificationHandler = notificationHandler;
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+            this.session = container.connectToServer(this, socketURI);
+
+            //set message handler
+            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+                @Override
+                public void onMessage(String message) {
+//                    ServerMessage serverMessage = new Gson().fromJson(message, ServerMessage.class);
+                    handleMessage(message);
+                }
+            });
+        } catch (DeploymentException | IOException | URISyntaxException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+
+    //Endpoint requires this method, but you don't have to do anything
+    @Override
+    public void onOpen(Session session, EndpointConfig endpointConfig) {
+    }
+
+    private void handleMessage(String message) {
+        char messageType = message.split(":")[1].charAt(1);
+        switch (messageType) {
+            case 'N' -> {
+                handleNotification(message);
+            }
+            case 'E' -> {
+                handleError(message);
+            }
+            case 'L' -> {
+                handleLoadGame(message);
+            }
+        }
+    }
+
+    private void handleNotification(String message) {
+        notificationHandler.notify("Notify not implemented");
+    }
+
+    private void handleError(String message) {
+        notificationHandler.notify("Error not implemented");
+    }
+
+    private void handleLoadGame(String message) {
+        notificationHandler.notify("Load game not implemented");
+    }
+
+    public void connectGame(String authToken, int gameID) throws ResponseException {
+        try {
+            var action = new UserGameCommand(UserGameCommand.CommandType.CONNECT, authToken, gameID);
+            this.session.getBasicRemote().sendText(new Gson().toJson(action));
+        } catch (IOException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+
 }
